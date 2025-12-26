43cc01edb29e77e10055e639cc8fb9c439d178cd
therahmonqulov <therahmonqulov@gmail.com>
2025-12-26 16:53:54 +0500

    add

--- public/script.js
+++ public/script.js
@@ -1,11 +1,13 @@
 // video
 const video = document.getElementById('myVideo');
 const playBtn = document.getElementById('playBtn');
+const iframeVideoText = document.querySelector(".iframe_video_text")
 
 playBtn.addEventListener('click', () => {
     video.play();
     video.setAttribute("controls", "controls");
     playBtn.style.display = "none";
+    iframeVideoText.style.display = "none"
 });
 
 // Video tugaganda play tugmasi yana chiqishi
