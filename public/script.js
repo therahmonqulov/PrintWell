dd9cc3d3c7f6c5721da8d259d0ae29e5daf10481
therahmonqulov <therahmonqulov@gmail.com>
2025-12-27 14:56:54 +0500

    +ad

--- public/script.js
+++ public/script.js
@@ -2,16 +2,37 @@
 const video = document.getElementById('myVideo');
 const playBtn = document.getElementById('playBtn');
 const iframeVideoText = document.querySelector(".iframe_video_text")
-
 playBtn.addEventListener('click', () => {
     video.play();
     video.setAttribute("controls", "controls");
     playBtn.style.display = "none";
     iframeVideoText.style.display = "none"
 });
-
 // Video tugaganda play tugmasi yana chiqishi
 video.addEventListener('ended', () => {
     playBtn.style.display = "flex";
     video.removeAttribute("controls");
 });
+
+// faq-item
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+    const icon = item.querySelector(".toggle-icon");
+
+    item.addEventListener("click", () => {
+        const isActive = item.classList.contains("active");
+
+        faqItems.forEach((el) => {
+            el.classList.remove("active");
+            el.querySelector(".toggle-icon").textContent = "-";
+        });
+
+        if (!isActive) {
+            item.classList.add("active");
+            icon.textContent = "+";
+        }
+    });
+});
+
+
